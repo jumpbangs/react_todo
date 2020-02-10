49059461549ece7787f8e0032b5f92999b9b0367
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-10 11:12:55 +0000

    Deploy jumpbangs/react_todo to github.com/jumpbangs/react_todo.git:gh-pages

---
+++ precache-manifest.da13776929d727ae33e2898e52eb88b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af56b8340c960057af75c5d2bdce927e",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "d09a1a51357c9565f965",
+    "url": "/react_todo/static/css/main.8d5e033b.chunk.css"
+  },
+  {
+    "revision": "0ac81a9eae593ced94dd",
+    "url": "/react_todo/static/js/2.6861c366.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_todo/static/js/2.6861c366.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d09a1a51357c9565f965",
+    "url": "/react_todo/static/js/main.26cbdef2.chunk.js"
+  },
+  {
+    "revision": "0f3a74f9669fe9cd5c90",
+    "url": "/react_todo/static/js/runtime-main.4205df89.js"
+  }
+]);

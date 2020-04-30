65bd249d895f120d91ea4e2e8bef61679289255d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-30 11:15:56 +0000

    Deploy jumpbangs/react_todo to github.com/jumpbangs/react_todo.git:gh-pages

---
+++ precache-manifest.896bade3766501d914d6ffc4643404e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7043a650fb9e37997a5ee55e78df5c40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40506c33292b6fb6948e",
+    "url": "/static/css/main.d11580e2.chunk.css"
+  },
+  {
+    "revision": "beb7d06883cbce49c352",
+    "url": "/static/js/2.0e2c8de9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0e2c8de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40506c33292b6fb6948e",
+    "url": "/static/js/main.187ea62f.chunk.js"
+  },
+  {
+    "revision": "f10b92a64592a689483d",
+    "url": "/static/js/runtime-main.a77afa4a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo/precache-manifest.7260a495cb337782eca2fe41193c7126.js"
+  "/precache-manifest.896bade3766501d914d6ffc4643404e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_todo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
